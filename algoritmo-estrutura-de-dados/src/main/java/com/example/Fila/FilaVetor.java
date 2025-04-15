33ecbdd1813f77dc8cf1bde475691abec607e9fe
Yasmin <yasminalvesdesouza27@gmail.com>
2025-04-14 23:22:29 -0300

    Fila com Implementação em Vetor

--- algoritmo-estrutura-de-dados/src/main/java/com/example/Fila/FilaVetor.java
+++ algoritmo-estrutura-de-dados/src/main/java/com/example/Fila/FilaVetor.java
@@ -36,20 +36,35 @@ public class FilaVetor implements Fila {
 
     @Override
     public Object peek() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+        if(estaVazia())
+            throw new FilaVaziaException();
+        return this.info[this.inicio];
     }
 
     @Override
     public Object retirar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'retirar'");
+        int valor = (int) peek();
+        this.inicio = (this.inicio + 1) % this.limite;
+        this.tamanho = this.tamanho - 1;
+        return valor;
     }
 
     @Override
     public void liberar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'liberar'");
+        info = new Object[limite];
+        tamanho = 0;
     }
     
+    public String toString(){
+        String resultado = "";
+
+        for (int i = tamanho - 1; i >= 0; i--) {
+            resultado += info[i];
+
+            if(i > 0)
+                resultado += ", ";
+        }
+
+        return resultado;
+    }
 }

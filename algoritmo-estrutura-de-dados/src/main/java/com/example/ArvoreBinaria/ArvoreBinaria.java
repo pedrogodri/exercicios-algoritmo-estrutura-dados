e1b69b48b506d1843b35eac9d361a4a2c7f6aaa5
Yasmin Victória Alves De Souza <yvasouza@furb.br>
2025-04-15 21:36:38 -0300

    Assinatura metodos arvoresBinarios

--- algoritmo-estrutura-de-dados/src/main/java/com/example/ArvoreBinaria/ArvoreBinaria.java
+++ algoritmo-estrutura-de-dados/src/main/java/com/example/ArvoreBinaria/ArvoreBinaria.java
@@ -1,5 +1,41 @@
 package com.example.ArvoreBinaria;
 
 public class ArvoreBinaria<T> {
+    
+    private NoArvoreBinaria<T> raiz;
 
+    public ArvoreBinaria() {}
+
+    public void setRaiz(NoArvoreBinaria<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public boolean estaVazia() {
+        return this.raiz == null;
+    }
+
+    public boolean pertence(T info) {
+        return false;
+    }
+
+    private boolean pertence(NoArvoreBinaria<T> no, T infp) {
+        return false;
+    }
+
+    public String toString(){
+        String resultado = "";
+        return resultado;
+    }
+
+    private String arvorePre(ArvoreBinaria<T> no) {
+        return null;
+    }
+
+    public int contarNos() {
+        return 0;
+    }
+
+    private int contarNos(NoArvoreBinaria<T> no) {
+        return 0;
+    }
 }
